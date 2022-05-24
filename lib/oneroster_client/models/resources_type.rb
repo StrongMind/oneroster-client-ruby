841ffcca80aedf5fed83f9d66ab9b82c76d1ac0f
Sonny Huynh <sonny.huynh@strongmind.com>
2022-05-24 14:31:04 -0700

    PLT-1795 Correct types for Multiple OneRoster Types

--- lib/oneroster_client/models/resources_type.rb
+++ lib/oneroster_client/models/resources_type.rb
@@ -26,7 +26,7 @@ module OneRosterClient
     # Attribute type mapping.
     def self.openapi_types
       {
-        :'resources' => :'Object'
+        :'resources' => :'Array<ResourceType>'
       }
     end
 
