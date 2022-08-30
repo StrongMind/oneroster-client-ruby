b91620343ce7cda1a1238d63f1ddd52391f1f5a1
Sonny Huynh <sonny.huynh@strongmind.com>
2022-08-30 16:36:28 -0700

    PLT-2578 Implement failing tests

--- lib/oneroster_client/api_client.rb
+++ lib/oneroster_client/api_client.rb
@@ -54,17 +54,22 @@ module OneRosterClient
       end
 
       unless response.success?
-        if response.timed_out?
-          fail ApiError.new('Connection timed out')
-        elsif response.code == 0
-          # Errors from libcurl will be made visible here
-          fail ApiError.new(:code => 0,
-                            :message => response.return_message)
-        else
-          fail ApiError.new(:code => response.code,
-                            :response_headers => response.headers,
-                            :response_body => response.body),
-               response.status_message
+        # if response.timed_out?
+        #   fail ApiError.new('Connection timed out')
+        # elsif response.code == 0
+        #   # Errors from libcurl will be made visible here
+        #   fail ApiError.new(:code => 0,
+        #                     :message => response.return_message)
+        # else
+        #   fail ApiError.new(:code => response.code,
+        #                     :response_headers => response.headers,
+        #                     :response_body => response.body),
+        #        response.status_message
+        # end
+        if response.code >= 500 and response.code < 600
+          fail ServerError.new
+        elsif response.code >= 400 and response.code < 500
+          fail ClientError.new
         end
       end
 
